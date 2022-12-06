4d6778c2b3973ed475215cc8e42c91496ad5ff4a
SujalBochkar <Sujalbochkar@gmail.com>
2022-12-06 22:09:40 +0530

    Create oops.py

---
+++ oops.py
@@ -0,0 +1,15 @@
+# object oriented programming
+# Abstraction   removing or hiding then extra data
+# Encapsulation  bring all under the same roof
+# polymorphism   multi tasking
+# Inheritence   aquring the property from the defined ones
+# 1 class   
+# 2 object
+ 
+#! class defination
+# class car:
+#     h = 10
+# maruti=car() object creation
+# print(maruti.h)  passing value
+
+# init helps the initialize
